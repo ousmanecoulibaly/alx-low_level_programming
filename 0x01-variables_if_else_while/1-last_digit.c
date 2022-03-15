c2e3f0a652fac323862ca2045e980c09a6cbc934
ousmanecoulibaly <ousmane.ibn.coulibaly@gmail.com>
2022-03-14 21:22:35 -0700

    I write a program that print all possible combinations

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - The last digit
+ * 
+ *Return: Always 0 (Success)
+ */
+int main(void)
+{
+int n;
+int o;
+
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+for (o = n; o > 9 || o < -9;)
+{
+o = o % 10;
+}
+printf("Last digit of %d is %d", n, o)
+if (o > 5)
+{
+printf(" and is greater than 5\n");
+}
+else
+if (o == 0)
+{
+printf(" and is 0\n");
+}
+else
+printf(" and is less than 6 and not 0\n");
+return (0)
+
+}
+

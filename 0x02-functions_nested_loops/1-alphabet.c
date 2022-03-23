02c15fa0a6660e120f4e56bd8533d885c40cb65e
ousmanecoulibaly <ousmane.ibn.coulibaly@gmail.com>
2022-03-23 01:11:39 -0700

    fix

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,13 @@
-/*
- * File: 1-alphabet.c
- * Auth: Maiga Ousmane Iboun Coulibaly
- */
-
 #include "main.h"
 /**
  * main - Prints the alphabet , in lowercase , followed by a new line.
  *
  * Return: Always 0 . (Success)
  */
-void print_alphabet(void);
-
+void print_alphabet(void)
 {
 int i;
 for (i = 'a' ; i <= 'z' ; i++)
-	_putchar(i);
+_putchar(i);
 _putchar('\n');
 }

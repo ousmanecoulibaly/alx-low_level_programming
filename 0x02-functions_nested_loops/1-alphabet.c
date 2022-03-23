9ec4d8d8ad67915d85b9a1e4d3de4e8cc5801ea3
ousmanecoulibaly <ousmane.ibn.coulibaly@gmail.com>
2022-03-23 00:26:12 -0700

    Fix program that prints alphabet with betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,10 +1,16 @@
+/*
+ * File: 1-alphabet.c
+ * Auth: Maiga Ousmane Iboun Coulibaly
+ */
+
 #include "main.h"
 /**
  * main - Prints the alphabet , in lowercase , followed by a new line.
  *
  * Return: Always 0 . (Success)
  */
-void print_alphabet(void)
+void print_alphabet(void);
+
 {
 int i;
 for (i = 'a' ; i <= 'z' ; i++)

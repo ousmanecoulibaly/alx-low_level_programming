d5acbf727d0291370ab010d6e5a8d3e0b83308ac
ousmanecoulibaly <ousmane.ibn.coulibaly@gmail.com>
2022-03-24 09:51:14 -0700

    a function thatprints all natural numbers

--- 0x02-functions_nested_loops/11-main.c
+++ 0x02-functions_nested_loops/11-main.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include <stdio.h>
+
 /**
  * main - Check the code.
  *
@@ -8,9 +8,11 @@
 int main(void)
 
 {
- int n;
- n = add(89, 9);
- printf("%d\n", n);
+ print_to_98(0);
+ print_to_98(98);
+ print_to_98(111);
+ print_to_98(81);
+ print_to_98(-10);
  return (0);
 
 }

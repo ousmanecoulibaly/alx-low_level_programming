27cdbd23fe223d86718977c92c0145235ed1c380
ousmanecoulibaly <ousmane.ibn.coulibaly@gmail.com>
2022-03-24 11:36:44 -0700

    Some fix

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet_x10 - prints 10 times the alphabet in lowercase , followed by a new line
+ * print_alphabet_x10 - prints 10 times the alphabet in lowercase.
  *
  * Return: Always 0 (Success) .
  */

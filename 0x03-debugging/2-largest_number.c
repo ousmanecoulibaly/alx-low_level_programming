0daa0fe11afca54142e59255757ee1b39fc26533
root <ousmane.ibn.coulibaly@gmail.com>
2022-04-01 12:08:41 +0200

    fix all

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,13 +10,27 @@
 int largest_number(int a, int b, int c)
 {
 int largest;
-if (a >= b && b >= c)
+if (a >= b)
+{
+if (a >= c)
 {
 largest = a;
 }
-else if (c <= a && b <= a)
+else
 {
-largest = a;
+largest = c;
+}
+}
+else
+{
+if (b >= c)
+{
+largest = b;
+}
+else
+{
+largest = c;
+}
 }
 return (largest);
 }

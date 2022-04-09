b107bbf2b5669b63019a893daaa7e0cefe85f873
root <ousmane.ibn.coulibaly@gmail.com>
2022-04-09 06:03:24 +0200

    I write a function that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,2 @@
+#This is the MORE MALLOC FREE repository
+It will contain all functions of programs or tasks about the Topic.
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,5 @@
+#ifndef MORE_MALLOC_FREE_HEADER_FILE
+#define MORE_MALLOC_FREE_HEADER_FILE
+void *malloc_checked(unsigned int b);
+
+#endif
